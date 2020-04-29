fbe3dad713534cf421700d2226dab62f760f9921
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-04-29 18:23:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,11 +26,19 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat'", "skittles"]
 
 function addElementToBeginningOfArray(a, b){
   return [b, ...a]
-
 }
 
 function destructivelyAddElementToBeginningOfArray(a, b){
   a.unshift(b)
   return a
-
 }
+
+function addElementToEndOfArray(a, b){
+  return [...a, b]
+}
+
+function destructivelyAddElementToEndOfArray(a, b){
+  a.push(b)
+  return a
+}
+
