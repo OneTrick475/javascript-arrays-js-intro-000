9b4bf4e95d2b2faf0c1c24208ea289f0225d1490
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-04-29 18:33:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -53,5 +53,9 @@ function destructivelyRemoveElementFromBeginningOfArray(a){
 
 function removeElementFromBeginningOfArray(a){
   return a.slice(1)
+  
+}
 
+function destructivelyRemoveElementFromEndOfArray(a){
+  return a.pop()
 }
